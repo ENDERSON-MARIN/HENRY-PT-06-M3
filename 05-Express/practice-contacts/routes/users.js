f14e8e2b88cb5a8ec3a692758402c813acfe95e2
Enderson Marín <46828206+ENDERSON-MARIN@users.noreply.github.com>
2022-06-21 10:15:00 -0500

    ADD .GITIGNORE FILE

--- 05-Express/practice-contacts/routes/users.js
+++ 05-Express/practice-contacts/routes/users.js
@@ -1,8 +1,10 @@
+/* PRACTICANDO CON UNA API EXTERNA MEDIANTE AXIOS */
 const { Router } = require("express");
 const axios = require("axios").default;
 
 const router = Router();
 
+/* CON PROMESAS .THEN().CATCH */
 router.get("/", (req, res) => {
   axios
     .get("https://jsonplaceholder.typicode.com/users")
@@ -10,10 +12,12 @@ router.get("/", (req, res) => {
     .catch(() => res.status(500).json({ message: "Se rompió con .then!" }));
 });
 
+/* CON ASYNC AWAIT, MANEJANDO EL ERROR MEDIANTE TRY CATCH */
 router.get("/:id", async (req, res) => {
+  const { id } = req.params;
   try {
     const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/users/" + req.params.id
+      "https://jsonplaceholder.typicode.com/users/" + parseInt(id)
     );
     res.json(response.data);
   } catch {
